1b45cff77ba76086dcc2ab74f79aa67174c7f420
Ella Costello <ella.costello@yahoo.com>
2026-06-11 23:38:09 +0200

    added init app

---
+++ src/main/java/com/hauxy/workoutlog/initApp/InitApp.java
@@ -0,0 +1,21 @@
+package com.hauxy.workoutlog.initApp;
+
+import com.hauxy.workoutlog.Workout.WorkoutLog;
+import com.hauxy.workoutlog.Workout.WorkoutLogRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class InitApp {
+    @Bean
+    CommandLineRunner initDatabase(WorkoutLogRepository workoutLogRepository) {
+
+        return args -> {
+            workoutLogRepository.save(new WorkoutLog("Squats", 10));
+            workoutLogRepository.save(new WorkoutLog("Pushups", 15));
+            workoutLogRepository.save(new WorkoutLog("Plank", 60));
+        };
+    }
+}
